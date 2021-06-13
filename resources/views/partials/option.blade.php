922ed0c5378dd4a3a072e7eebbbb3f257f574fad
Loris Leiva <loris.leiva@gmail.com>
2021-06-13 11:32:19 +0100

    wip

--- resources/views/partials/option.blade.php
+++ resources/views/partials/option.blade.php
@@ -1,7 +1,23 @@
 @php /** @var Symfony\Component\Console\Input\InputOption $option */ @endphp
 
-@include('artisan-ui::fields.boolean', [
-    'name' => $option->getName(),
-    'default' => $option->getDefault(),
-    'description' => $option->getDescription(),
-])
+@if($option->acceptValue() && $option->isArray())
+    @include('artisan-ui::fields.text', [
+        'name' => $option->getName(),
+        'default' => $option->getDefault(),
+        'description' => $option->getDescription(),
+        'required' => $option->isValueRequired(),
+    ])
+@elseif($option->acceptValue())
+    @include('artisan-ui::fields.text', [
+        'name' => $option->getName(),
+        'default' => $option->getDefault(),
+        'description' => $option->getDescription(),
+        'required' => $option->isValueRequired(),
+    ])
+@else
+    @include('artisan-ui::fields.boolean', [
+        'name' => $option->getName(),
+        'default' => $option->getDefault(),
+        'description' => $option->getDescription(),
+    ])
+@endif
