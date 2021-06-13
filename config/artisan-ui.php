5b917680ad527f671a83ff56fe1be634965a7056
Loris Leiva <loris.leiva@gmail.com>
2021-06-13 18:04:22 +0100

    wip

--- config/artisan-ui.php
+++ config/artisan-ui.php
@@ -1,5 +1,42 @@
 <?php
 
 return [
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Artisan UI Domain
+    |--------------------------------------------------------------------------
+    |
+    | This is the subdomain where Artisan UI will be accessible from. If this
+    | setting is null, Artisan UI will reside under the same domain as the
+    | application. Otherwise, this value will serve as the subdomain.
+    |
+    */
+
+    'domain' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Artisan UI Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where the Artisan UI pages will be accessible from.
+    | All Artisan UI routes will be prefixed by this path. E.g. "/artisan",
+    | "/artisan/migrate". Feel free to change this to anything you like.
+    |
+    */
+
+    'path' => 'artisan',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Artisan UI Route Middleware
+    |--------------------------------------------------------------------------
+    |
+    | These middleware will get attached to each Artisan UI route, allowing
+    | you to add your own middleware to this list or change any of the
+    | existing middleware. Or, you can simply stick with this list.
+    |
+    */
+
+    'middleware' => ['web'],
 ];
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,14 @@ use Lorisleiva\ArtisanUI\Actions\ExecuteArtisanCommand;
 use Lorisleiva\ArtisanUI\Actions\ShowArtisanCommand;
 use Lorisleiva\ArtisanUI\Actions\ShowArtisanUI;
 
-Route::get('/artisan', ShowArtisanUI::class)->name('artisan-ui.home');
-Route::get('/artisan/{name}', ShowArtisanCommand::class)->name('artisan-ui.detail');
-Route::post('/artisan/{name}/execution', ExecuteArtisanCommand::class)->name('artisan-ui.execution');
+
+Route::group([
+    'domain' => config('artisan-ui.domain', null),
+    'prefix' => config('artisan-ui.path'),
+    'middleware' => config('artisan-ui.middleware', 'web'),
+    'as' => 'artisan-ui.',
+], function () {
+    Route::get('/', ShowArtisanUI::class)->name('home');
+    Route::get('/{name}', ShowArtisanCommand::class)->name('detail');
+    Route::post('/{name}/execution', ExecuteArtisanCommand::class)->name('execution');
+});
