e599bf75ca7eec27df056fde044502bbda77a1e5
Loris Leiva <loris.leiva@gmail.com>
2021-06-13 18:37:16 +0100

    wip

--- src/Http/Middleware/AuthorizeArtisanUI.php
+++ src/Http/Middleware/AuthorizeArtisanUI.php
@@ -4,12 +4,11 @@ namespace Lorisleiva\ArtisanUI\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Lorisleiva\ArtisanUI\Facades\ArtisanUI;
 
 class AuthorizeArtisanUI
 {
-    public function handle(Request $request, Closure $next): ?Response
+    public function handle(Request $request, Closure $next)
     {
         return ArtisanUI::check($request) ? $next($request) : abort(403);
     }
