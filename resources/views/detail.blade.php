4f47486d4c89cab23a34c6961d38a6f9740a3443
Loris Leiva <loris.leiva@gmail.com>
2021-06-13 13:43:52 +0100

    wip

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,46 +2,48 @@
 @php /** @var Illuminate\Console\Command $command */ @endphp
 
 @section('content')
-    <header class="mb-6">
-        <h1 class="text-3xl font-mono">{{ $command->getName() }}</h1>
-        <p class="text-gray-700">{{ $command->getDescription() }}</p>
-    </header>
+    <div x-data>
+        <header class="mb-6">
+            <h1 class="text-3xl font-mono">{{ $command->getName() }}</h1>
+            <p class="text-gray-700">{{ $command->getDescription() }}</p>
+        </header>
 
-    <div x-data="{ open: false }" class="mb-4">
-        @include('artisan-ui::partials.accordion-button', [
-            'title' => sprintf('Arguments (%s)', count($command->getDefinition()->getArguments())),
-            'disabled' => empty($command->getDefinition()->getArguments()),
-        ])
+        <div x-data="{ open: false }" class="mb-4">
+            @include('artisan-ui::partials.accordion-button', [
+                'title' => sprintf('Arguments (%s)', count($command->getDefinition()->getArguments())),
+                'disabled' => empty($command->getDefinition()->getArguments()),
+            ])
 
-        @unless(empty($command->getDefinition()->getArguments()))
-            <ul x-show="open" class="space-y-4 py-4">
-                @foreach($command->getDefinition()->getArguments() as $argument)
-                    <li>
-                        @include('artisan-ui::.partials.argument', compact('argument'))
-                    </li>
-                @endforeach
-            </ul>
-        @endunless
+            @unless(empty($command->getDefinition()->getArguments()))
+                <ul x-show="open" class="space-y-4 py-4">
+                    @foreach($command->getDefinition()->getArguments() as $argument)
+                        <li>
+                            @include('artisan-ui::.partials.argument', compact('argument'))
+                        </li>
+                    @endforeach
+                </ul>
+            @endunless
+        </div>
+
+        <div x-data="{ open: false }" class="mb-4">
+            @include('artisan-ui::partials.accordion-button', [
+                'title' => sprintf('Options (%s)', count($command->getDefinition()->getOptions())),
+                'disabled' => empty($command->getDefinition()->getOptions()),
+            ])
+
+            @unless(empty($command->getDefinition()->getOptions()))
+                <ul x-show="open" class="space-y-4 py-4">
+                    @foreach($command->getDefinition()->getOptions() as $option)
+                        <li>
+                            @include('artisan-ui::.partials.option', compact('option'))
+                        </li>
+                    @endforeach
+                </ul>
+            @endunless
+        </div>
+
+        <button class="px-6 py-2 bg-gray-700 text-white rounded" @click="() => axios.post('{{ route('artisan-ui.execution', $command->getName()) }}')">
+            Execute
+        </button>
     </div>
-
-    <div x-data="{ open: false }" class="mb-4">
-        @include('artisan-ui::partials.accordion-button', [
-            'title' => sprintf('Options (%s)', count($command->getDefinition()->getOptions())),
-            'disabled' => empty($command->getDefinition()->getOptions()),
-        ])
-
-        @unless(empty($command->getDefinition()->getOptions()))
-            <ul x-show="open" class="space-y-4 py-4">
-                @foreach($command->getDefinition()->getOptions() as $option)
-                    <li>
-                        @include('artisan-ui::.partials.option', compact('option'))
-                    </li>
-                @endforeach
-            </ul>
-        @endunless
-    </div>
-
-    <button class="px-6 py-2 bg-gray-700 text-white rounded">
-        Execute
-    </button>
 @endsection
