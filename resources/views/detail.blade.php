4ff39e14663fccce6cb8a75841ef08d6999cc569
Loris Leiva <loris.leiva@gmail.com>
2021-06-13 15:15:17 +0100

    wip

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,7 +2,7 @@
 @php /** @var Illuminate\Console\Command $command */ @endphp
 
 @section('content')
-    <div x-data>
+    <div x-data="command">
         <header class="mb-6">
             <h1 class="text-3xl font-mono">{{ $command->getName() }}</h1>
             <p class="text-gray-700">{{ $command->getDescription() }}</p>
@@ -42,8 +42,59 @@
             @endunless
         </div>
 
-        <button class="px-6 py-2 bg-gray-700 text-white rounded" @click="() => axios.post('{{ route('artisan-ui.execution', $command->getName()) }}')">
-            Execute
+        <button
+            class="relative px-6 py-2 bg-gray-700 text-white rounded"
+            :disabled="state === 'loading'"
+            @click="execute"
+        >
+            <div x-show="state === 'loading'" x-cloak class="absolute inset-0 bg-gray-500 text-white rounded flex items-center justify-center cursor-not-allowed">
+                <svg class="animate-spin w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-100" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </div>
+            <div>Execute</div>
         </button>
+
+        <pre class="mt-16 bg-gray-700 text-white rounded p-8" x-text="output"></pre>
     </div>
+
+    <script>
+        document.addEventListener('alpine:initializing', () => {
+            Alpine.data('command', () => ({
+                state: 'idle',
+                arguments: {!!
+                    collect($command->getDefinition()->getArguments())
+                        ->mapWithKeys(fn ($value) => [$value->getName() => null])
+                        ->toJson()
+                !!},
+                options: {!!
+                    collect($command->getDefinition()->getOptions())
+                        ->mapWithKeys(fn ($value) => [$value->getName() => null])
+                        ->toJson()
+                !!},
+                route: '{{ route('artisan-ui.execution', $command->getName()) }}',
+                output: '',
+
+                execute () {
+                    if (this.state === 'loading') return
+                    this.state = 'loading'
+
+                    axios.post(this.route)
+                        .then(this.onSuccess.bind(this))
+                        .catch(this.onFailure.bind(this))
+                },
+
+                onSuccess (response) {
+                    this.output = response.data.output || ''
+                    this.state = response.data.success ? 'success' : 'error'
+                },
+
+                onFailure (error) {
+                    this.output = error.response.data.message || 'Something went wrong'
+                    this.state = 'error'
+                },
+            }))
+        })
+    </script>
 @endsection
--- src/Actions/ExecuteArtisanCommand.php
+++ src/Actions/ExecuteArtisanCommand.php
@@ -2,22 +2,29 @@
 
 namespace Lorisleiva\ArtisanUI\Actions;
 
+use Illuminate\Console\Command;
+use Illuminate\Http\JsonResponse;
 use Lorisleiva\ArtisanUI\ArtisanUI;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 class ExecuteArtisanCommand
 {
-    public function __invoke(string $name, ArtisanUI $artisanUI): string
+    public function __invoke(string $name, ArtisanUI $artisanUI): JsonResponse
     {
         $command = $artisanUI->findOrFail($name);
 
         $input = new ArrayInput([
-            'name' => 'SomeEvent',
+            // 'name' => 'SomeEvent',
         ]);
-        $output = new BufferedOutput();
-        $command->run($input, $output);
 
-        return $output->fetch();
+        // $output = new StreamOutput(fopen(__DIR__ . '/output.log', 'a', false));
+        $output = new BufferedOutput();
+        $code = $command->run($input, $output);
+
+        return response()->json([
+            'success' => $code === Command::SUCCESS,
+            'output' => $output->fetch(),
+        ]);
     }
 }
