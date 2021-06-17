6176c6fb84c5dbbd019a9d62f5bfbe49c88e5786
adithyasrinivasan <sharks12@live.in>
2021-06-17 11:51:04 +0200

    Update CommandArtisanUITest.php

--- tests/CommandArtisanUITest.php
+++ tests/CommandArtisanUITest.php
@@ -7,18 +7,18 @@ namespace Lorisleiva\ArtisanUI\Tests;
 use Lorisleiva\ArtisanUI\ArtisanUI;
 use Lorisleiva\ArtisanUI\Command;
 
-it('Accordion Closed when there are no arguments needed', function () {
+it('closes the accordion when there are no arguments', function () {
     $artisanCommand = (new ArtisanUI())->find('config:cache');
     $this->assertFalse($artisanCommand->shouldOpenArgumentsAccordionOnLoad());
 });
 
-it('Accordion closed when no arguments are required', function () {
+it('closes the accordion when no arguments are required', function () {
     $artisanCommand = (new ArtisanUI())->find('cache:clear');
     $this->assertFalse($artisanCommand->shouldOpenArgumentsAccordionOnLoad());
 });
 
 
-it('Accordion open when arguments are required', function () {
+it('opens the accordion when there are required arguments', function () {
     $artisanCommand = (new ArtisanUI())->find('make:model');
     $this->assertTrue($artisanCommand->shouldOpenArgumentsAccordionOnLoad());
 });
