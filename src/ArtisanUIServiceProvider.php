1437b72ad63e30a0e3a67d98bb505084a4f9659a
Manuel <fodormanuel@gmail.com>
2023-05-31 14:07:29 +0200

    Remove import

--- src/ArtisanUIServiceProvider.php
+++ src/ArtisanUIServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Lorisleiva\ArtisanUI;
 
-use Illuminate\Foundation\Console\AboutCommand;
 use Lorisleiva\ArtisanUI\Commands\ArtisanUIInstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
